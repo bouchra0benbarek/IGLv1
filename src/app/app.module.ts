55d8c7f1c81d77a54861e6e159ae36124def20fb
Benbarek Bouchra <hb_benbarek@esi.dz>
2019-11-25 23:11:27 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { ChoicesComponent } from './choices/choices.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChoicesComponent,
-    SidebarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
